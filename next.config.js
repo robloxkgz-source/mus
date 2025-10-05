c0de5585af7447ca8d42f7f7560371d5296f539d
robloxkgz-source <robloxkgz@gmail.com>
2025-10-05 23:52:52 +0600

    Fix image paths

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  basePath: isProd ? "/Crypgo" : "",
+  assetPrefix: isProd ? "/Crypgo/" : "",
+
   images: {
-    unoptimized: true, // если не используешь Image Optimization
+    unoptimized: true,
   },
 };
 
 export default nextConfig;
-
  
