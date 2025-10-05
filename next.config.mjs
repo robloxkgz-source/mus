ed1bc2ce293e40786a2f779e7cd49bf32ae51a8c
robloxkgz-source <robloxkgz@gmail.com>
2025-10-05 22:53:30 +0600

    remove next export to fix Tailwind styles

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   basePath: isProd ? "/Crypgo" : "",
   assetPrefix: isProd ? "/Crypgo/" : "",
-  output: "export",
+  
   images: {
     unoptimized: true,
   },
