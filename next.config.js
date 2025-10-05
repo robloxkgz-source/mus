ddca4f4a47d0d8358bdb59770dbe6dddc1053a6b
robloxkgz-source <robloxkgz@gmail.com>
2025-10-05 23:27:10 +0600

    fix: remove basePath and assetPrefix from next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true, // если не используешь Image Optimization
+  },
+};
+
+export default nextConfig;
+
+ 
